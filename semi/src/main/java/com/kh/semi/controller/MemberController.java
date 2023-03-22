1297dba30c581bda0000e5b42877d05bd3163b74
Moon-Tree <Ellaya1012@gmail.com>
2023-03-22 16:41:16 +0900

    개인정보 수정 기능 코드 추가

--- semi/src/main/java/com/kh/semi/controller/MemberController.java
+++ semi/src/main/java/com/kh/semi/controller/MemberController.java
@@ -89,6 +89,35 @@ public class MemberController {
        model.addAttribute("memberDto", memberDto);
        return "/WEB-INF/views/member/edit.jsp";
     }
+    
+	 //개인정보 변경 처리 기능
+	 @PostMapping("/edit")
+	 public String edit(
+			 @ModelAttribute MemberDto memberDto, //데이터 자동 수신 객체
+			 HttpSession session, //회원 아이디가 저장되어 있는 세션 객체
+			 RedirectAttributes attr //리다이렉트 시 정보를 추가할 전송 객체
+		 ) {
+		 String memberId = (String)session.getAttribute("memberId");
+		 MemberDto findDto = memberDao.selectOne(memberId);
+
+		 //비밀번호가 일치하지 않는다면 → 에러 표시 후 이전 페이지로 리다이렉트
+		 if(!findDto.getMemberPw().equals(memberDto.getMemberPw())) {
+			 attr.addAttribute("mode", "error");
+			 return "redirect:edit";
+		 }
+
+		 //비밀번호가 일치한다면 → 비밀번호 변경 및 완료 페이지로 리다이렉트
+		 memberDto.setMemberId(memberId); //아이디를 추가 설정
+		 memberDao.changeInformation(memberDto); //정보 변경 요청
+		 return "redirect:editFinish";
+	 }
+	 
+	 //개인정보 변경 완료 페이지
+	 @GetMapping("/editFinish")
+	 public String editFinish() {
+		 return "/WEB-INF/views/member/editFinish.jsp";
+	 }
+    
 		
 //	 회원 탈퇴
 	 @GetMapping("/exit")
