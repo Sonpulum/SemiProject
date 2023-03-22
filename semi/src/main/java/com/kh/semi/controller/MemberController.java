07c847194353d891e77c62a75af0034ea7f1ddea
juhyun07 <qkrwngus10@naver.com>
2023-03-22 15:49:08 +0900

    마이페이지,로그인,회원정보 구현

--- semi/src/main/java/com/kh/semi/controller/MemberController.java
+++ semi/src/main/java/com/kh/semi/controller/MemberController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -37,6 +38,58 @@ public class MemberController {
 		 return "/WEB-INF/views/member/joinFinish.jsp";
 	 }
 	 
+	 //로그인
+	 @GetMapping("/login")
+	    public String login(HttpSession session) {
+	       return "/WEB-INF/views/member/login.jsp";
+	    }
+	    
+	 @PostMapping("/login")
+	    public String login(
+	          @ModelAttribute MemberDto userDto,
+	          RedirectAttributes attr, HttpSession session) {
+
+	       MemberDto findDto = memberDao.selectOne(userDto.getMemberId());
+	       
+	       if(findDto == null) {
+	          attr.addAttribute("mode", "error");
+	          return "redirect:login";
+	       }
+	       
+	       if(!userDto.getMemberPw().equals(findDto.getMemberPw())) {
+	          attr.addAttribute("mode", "error");
+	          return "redirect:login";
+	       }
+	       
+	       session.setAttribute("memberId", findDto.getMemberId());
+	       session.setAttribute("memberLevel", findDto.getMemberLevel());
+	       
+	       return "redirect:/";
+	    }
+
+	 
+	//마이페이지
+		@GetMapping("/mypage")
+		public String mypage(Model model, HttpSession session) {
+				String memberId = (String)session.getAttribute("memberId");
+				MemberDto memberDto = memberDao.selectOne(memberId);
+				model.addAttribute("memberDto",memberDto);
+				
+				return "/WEB-INF/views/member/mypage.jsp";
+		}
+		
+	//비밀번호를 제외한 나머지 개인정보 변경
+    @GetMapping("/edit")
+    public String edit(
+          HttpSession session,
+          Model model
+         ) {
+       String memberId = (String) session.getAttribute("memberId");
+       MemberDto memberDto = memberDao.selectOne(memberId);
+       model.addAttribute("memberDto", memberDto);
+       return "/WEB-INF/views/member/edit.jsp";
+    }
+		
 //	 회원 탈퇴
 	 @GetMapping("/exit")
 	 public String exit(HttpSession session) {
